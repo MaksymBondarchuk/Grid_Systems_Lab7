1b8c9fec5837be25887377230d44fdfe47928036
MaxBondarchuk <bondarchuk.m.y@gmail.com>
2017-06-05 18:06:45 +0300

    Debugging.... v2

--- main.cpp
+++ main.cpp
@@ -34,14 +34,12 @@ public:
             f(f_),
             epsilon(epsilon_) {}
 
-    bool isnan_double(double f) const { return (f != f); }
-
     double operator()(const blocked_range<double> &r, double start) const {
-        int num_iterations = 1; /* Початкова кількість ітерацій */
+        long long num_iterations = 1; /* Початкова кількість ітерацій */
         double last_res = 0.;   /* Результат інтeгрування на попередньому кроці */
         double res = -1.;       /* Поточний результат інтегрування */
-        double h = 0.;          /* Ширина прямокутників */
-        cout << "Start=" << r.begin() << " End=" << r.end() << "; " << endl;
+        long double h = 0.;          /* Ширина прямокутників */
+        cout << "Start=" << r.begin() << " End=" << r.end() << " epsilon=" << epsilon << endl;
         while (!check_Runge(res, last_res, epsilon)) {
             num_iterations *= 2;
             last_res = res;
@@ -49,13 +47,14 @@ public:
             h = (r.end() - r.begin()) / num_iterations;
             for (long long i = 0; i < num_iterations; i++) {
                 res += f(r.begin() + i * h);
-                if (isnan_double(res)) {
-                    cout << "res=" << f(r.begin() + i * h) << endl;
-                }
+//                if (isnan_double(res)) {
+//                    cout << "res=" << f(r.begin() + i * h) << endl;
+                    cout << "delta=" << fabs(res - last_res) / 3. << " num_iterations" << num_iterations << endl;
+//                }
             }
 //            cout << "Iterations=" << num_iterations << " Res=" << res << "; " << last_res << endl;
         }
-        cout << "start=" << start << " res=" << res << endl;
+        cout << "start=" << start << " res=" << res << " h=" << h << " num_iterations=" << num_iterations << endl;
         res *= h;
         cout << "start=" << start << " res=" << res << endl;
         return start + res;
@@ -64,6 +63,44 @@ public:
     Integrand f;
     double epsilon;
 };
+
+template<typename Integrand>
+class integrate_left_rectangle_func1 {
+public:
+    integrate_left_rectangle_func1(Integrand f_, double epsilon_) :
+            f(f_),
+            epsilon(epsilon_) {}
+
+    double run(double a, double b) const {
+        long long num_iterations = 1; /* Початкова кількість ітерацій */
+        double last_res = 0.;   /* Результат інтeгрування на попередньому кроці */
+        double res = -1.;       /* Поточний результат інтегрування */
+        long double h = 0.;          /* Ширина прямокутників */
+        cout << "Start=" << a << " End=" << b << " epsilon=" << epsilon << endl;
+        while (!check_Runge(res, last_res, epsilon)) {
+            num_iterations *= 2;
+            last_res = res;
+            res = 0.;
+            h = (b - a) / num_iterations;
+            for (long long i = 0; i < num_iterations; i++) {
+                res += f(a + i * h);
+//                if (isnan_double(res)) {
+//                    cout << "res=" << f(r.begin() + i * h) << endl;
+                cout << "delta=" << fabs(res - last_res) / 3. << " num_iterations" << num_iterations << endl;
+//                }
+            }
+//            cout << "Iterations=" << num_iterations << " Res=" << res << "; " << last_res << endl;
+        }
+        cout << "res=" << res << " h=" << h << " num_iterations=" << num_iterations << endl;
+        res *= h;
+        cout << "res=" << res << endl;
+        return res;
+    }
+
+    Integrand f;
+    double epsilon;
+};
+
 
 /*
  * Об'єкт-функція, що виконує додавання двох елементів
@@ -86,15 +123,18 @@ double parallel_integrate_left_rectangle(
 }
 
 int main() {
-    const int P_max = task_scheduler_init::default_num_threads();
-    cout << "Start. P_max=" << P_max << endl;
-    for (int P = 1; P <= P_max; P++) {
-        task_scheduler_init init(P);
-        tick_count t0 = tick_count::now();
-        cout << parallel_integrate_left_rectangle(integrand(), 0, 5, 1e-3)
-             << endl;
-        tick_count t1 = tick_count::now();
-        double t = (t1 - t0).seconds();
-        cout << "time = " << t << " with " << P << " threads" << endl;
-    }
+    integrate_left_rectangle_func1<integrand> iii = integrate_left_rectangle_func1<integrand>(integrand(), 1e-2);
+    cout << iii.run(0, 5) << endl;
+
+//    const int P_max = task_scheduler_init::default_num_threads();
+//    cout << "Start. P_max=" << P_max << endl;
+//    for (int P = 1; P <= P_max; P++) {
+//        task_scheduler_init init(P);
+//        tick_count t0 = tick_count::now();
+//        cout << parallel_integrate_left_rectangle(integrand(), 0, 5, 1e-2)
+//             << endl;
+//        tick_count t1 = tick_count::now();
+//        double t = (t1 - t0).seconds();
+//        cout << "time = " << t << " with " << P << " threads" << endl;
+//    }
 }
